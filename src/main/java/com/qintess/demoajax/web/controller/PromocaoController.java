828059e1fe9656da4f364a6a4645777453ea175a
Marcelo S Silva <mrsalustiano@gmail.com>
2020-05-03 01:48:34 -0300

    scroll infinite e acerto trim de titulo e descricao

--- src/main/java/com/qintess/demoajax/web/controller/PromocaoController.java
+++ src/main/java/com/qintess/demoajax/web/controller/PromocaoController.java
@@ -10,14 +10,18 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.qintess.demoajax.domain.Categoria;
 import com.qintess.demoajax.domain.Promocao;
@@ -43,6 +47,7 @@ public class PromocaoController {
 		return categoriaRepository.findAll();
 	}
 	
+	//adicionar ofertas
 	@PostMapping("/save")
 	public ResponseEntity<?> salvarPromocao(@Valid Promocao promocao, BindingResult result){
 		
@@ -69,4 +74,21 @@ public class PromocaoController {
 		return "promo-add";
 	}
 	
+	// listar ofertas
+	
+	@GetMapping("/list")
+	public String listarOfertas(ModelMap model) {
+		Sort sort = Sort.by(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(0, 8, sort);		
+		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
+		return "promo-list";
+	}
+	
+	@GetMapping("/list/ajax")
+	public String listarCards(@RequestParam(name = "page", defaultValue = "1") int page,  ModelMap model) {
+		Sort sort = Sort.by(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(page, 8, sort);		
+		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
+		return "promo-card";
+	}
 }
